8c0a66d573a2312c6d30281c155d19e876b63f1e
David Tippett <17506770+dtaivpp@users.noreply.github.com>
2021-10-01 23:55:05 -0400

    Added livelyness poller and fixed async index creation

--- github-collector/runner.py
+++ github-collector/runner.py
@@ -4,13 +4,18 @@ Module used to run the collector and indexers
 import asyncio
 from indexer import index_batch, create_index, create_client
 from collector import repo_collector
+from helpers import livelyness_poller
 
 def main():
   """Main method for github-collector"""
   opensearch = create_client()
+
+  livelyness_poller(opensearch)
+
+  create_index(opensearch)
+  opensearch_as = create_client(isasync=True)
   loop = asyncio.get_event_loop()
-  loop.run_until_complete(create_index(opensearch))
-  loop.run_until_complete(index_batch(opensearch, repo_collector))
+  loop.run_until_complete(index_batch(opensearch_as, repo_collector))
 
 if __name__=="__main__":
   main()
